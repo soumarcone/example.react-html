8faa35a76ba00de25148bc67d8922890d3258f97
Marcone Ramos <sou.marcone@gmail.com>
2023-04-17 12:52:22 -0300

    feat: exemplo simples de comoo rodar o React 17 + Babel

---
+++ js/react.js
@@ -0,0 +1,13 @@
+function Corretores() {
+    const [count, setCount] = React.useState(0);
+    return (
+      <div>
+        <p>FORMULÁRIO REACT AQUI</p>
+      </div>
+    );
+  }
+
+
+const rootElement = document.getElementById("interactive-form");
+
+ReactDOM.render(<Corretores />, rootElement);
